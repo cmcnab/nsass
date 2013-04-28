58da2aa4317a2e45aa776a41ab35ba2c52d62a4d
Chadwick McNab <cmcnab@gmail.com>
2013-04-28 14:55:24 -0500

    Starting to work on syntax exception information.

---
+++ NSass.Tests/TestSyntaxExceptions.cs
@@ -0,0 +1,29 @@
+﻿namespace NSass.Tests
+{
+    using NSass.Parse;
+    using Xunit;
+
+    public class TestSyntaxExceptions
+    {
+        [Fact]
+        public void MissingCloseParenThrowsSyntaxException()
+        {
+            // Arrange
+            var engine = new Engine();
+            var input = @"#main {";
+
+            // Act/Assert
+            var ex = Assert.Throws<SyntaxException>(() => engine.Compile(input));
+            Assert.Equal(FormatErrorMessage("#main {", "}", string.Empty), ex.Message);
+        }
+
+        private static string FormatErrorMessage(string context, string expected, string actual)
+        {
+            return string.Format(
+                "Syntax error: Invalid CSS after \"{0}\": expected \"{1}\", was \"{2}\"",
+                context,
+                expected,
+                actual);
+        }
+    }
+}
